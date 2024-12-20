9d2fcf8d4c6bbf3066b9e41b49f6b81350c2d8e6
kou1924 <koryoma.22@gmail.com>
2024-12-21 02:17:40 +0900

    コメントアウトを修正

--- AdventCalendar2024.py
+++ AdventCalendar2024.py
@@ -1,15 +1,15 @@
 from streamlit_lottie import st_lottie
 import json
 
-# lottieJSONファイルを読み込む関数
+# JSONファイルを読み込む関数
 def load_lottie(file_path):
         with open(file_path, "r") as f:
                 return json.load(f)
 
-# ローカルにあるlottieJSONファイルのパスを指定
+# ローカルにあるJSONファイルのパスを指定
 file_path = "AdventCalendar2024.json"
 
-# ローカルにあるlottieファイルを読み込む
+# ローカルにあるJSONファイルを読み込む
 animation = load_lottie(file_path)
 
 st_lottie(animation, speed = 1, width = 600, height = 400)
